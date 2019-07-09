30913a849ab90eda5ae0ea5193d187927c7b7b35
Zoltan Ujvary <ujvzolee@gmail.com>
2019-07-09 20:57:38 +0200

    Travis deploy from script

---
+++ scripts/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export DOCKER_IMAGE=lunchboxlambda/lunchbox:0.0.1
+docker run --rm --privileged multiarch/qemu-user-static:register --reset
+docker build --build-arg BUILD_VERSION=$TRAVIS_TAG -t $DOCKER_IMAGE .
+echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
+docker push $DOCKER_IMAGE
