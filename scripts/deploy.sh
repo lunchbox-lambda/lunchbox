e6fca16eea5c4ae4a4088beae84cb50dd344675b
Zoltan Ujvary <ujvzolee@gmail.com>
2019-07-09 21:32:02 +0200

    Docker image tag from env

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export DOCKER_IMAGE=lunchboxlambda/lunchbox:0.0.1
+export DOCKER_IMAGE=lunchboxlambda/lunchbox:$TRAVIS_TAG
 docker run --rm --privileged multiarch/qemu-user-static:register --reset
 docker build --build-arg BUILD_VERSION=$TRAVIS_TAG -t $DOCKER_IMAGE .
 echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
